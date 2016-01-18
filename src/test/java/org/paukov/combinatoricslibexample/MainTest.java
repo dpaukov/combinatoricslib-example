0c8f5c9559f4dc4732840dae0cd79292f9e4b7ef
dpaukov <d.paukov@gmail.com>
2016-01-17 19:26:38 -0800

    Add a test to run the main method

---
+++ src/test/java/org/paukov/combinatoricslibexample/MainTest.java
@@ -0,0 +1,12 @@
+package org.paukov.combinatoricslibexample;
+
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void test_main(){
+        Main.main(new String[]{});
+    }
+}
